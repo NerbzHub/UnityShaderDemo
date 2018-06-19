8cb81fe5f30e6286899167dc9cc9ee6cce61964f
Nerbz <nathan.nette@gmail.com>
2018-06-19 22:29:17 +1000

    Pushing the unity files
    Currently have clank(from Bruno) in the scene and configured the character's movement to look realistic.

---
+++ ComputerGraphicsProject/ProjectComputerGraphics/Assets/Scripts/AnimController.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+
+public class AnimController : MonoBehaviour
+{
+
+    Animator m_Animator;
+    //int m_AnimState = 0;
+    private int AnimState;
+    private bool isWalking;
+    private bool isRunning;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        m_Animator = GetComponent<Animator>(); // gets animator component
+
+        //m_Animator.SetInteger(m_AnimState, 0); //sets state to 0 at beginning
+
+        //AnimState = m_Animator.GetInteger(AnimState);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W)
+            || Input.GetKey(KeyCode.A)
+            || Input.GetKey(KeyCode.S)
+            || Input.GetKey(KeyCode.D))
+        {
+            m_Animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            m_Animator.SetBool("isWalking", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            m_Animator.SetBool("isRunning", true);
+        }
+        else
+        {
+            m_Animator.SetBool("isRunning", false);
+        }
+
+
+        //if (Input.GetKeyDown(KeyCode.Alpha0))
+        //{
+
+        //    m_Animator.SetInteger("AnimState", 0);
+
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+
+        //    m_Animator.SetInteger("AnimState", 1);
+
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Alpha3))
+        //{
+
+        //    m_Animator.SetInteger("AnimState", 2);
+
+        //}
+
+    }
+}
---
+++ ComputerGraphicsProject/ProjectComputerGraphics/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//-------------------------------------------------------------------------------------
+// Class:
+//      PlayerController: Player controller controls the player's movment and most
+//                        of the interactions that the player has in the world.
+//-------------------------------------------------------------------------------------
+public class PlayerController : MonoBehaviour
+{
+    // Player's rigidbody.
+    public Rigidbody m_rigidBody;
+
+    // Rotational on the Y axis
+    public float rotationSpeed;
+
+    // Rate that the player moves at.
+    public float moveSpeed;
+
+    // Rate that the player runs at.
+    public float runSpeed;
+
+    // The maximum speed the player can go at.
+    public float maxSpeed = 12.0f;
+
+    // Able to rotate on the Y
+    Vector3 m_EulerAngleVelocity;
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //// If there is a gameobject with a rigidbody, freeze its' rotation.
+        //if (gameObject.GetComponent<Rigidbody>())
+        //    gameObject.GetComponent<Rigidbody>().freezeRotation = true;
+
+        //Set the axis the Rigidbody rotates in (100 in the y axis)
+        m_EulerAngleVelocity = new Vector3(0, 100, 0);
+    }
+
+    //-------------------------------------------------------------------------------------
+    // Update is called once per frame.
+    //-------------------------------------------------------------------------------------
+    void Update()
+    {
+        //-------------------------------------------------------------------------------------------
+        //                                   Keyboard Input Movement
+        //
+        if (Input.GetKey(KeyCode.W))
+        {
+            m_rigidBody.AddForce(new Vector3(transform.forward.x * moveSpeed, 0, transform.forward.z * moveSpeed), ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_rigidBody.AddForce(new Vector3(-transform.forward.x * moveSpeed, 0, -transform.forward.z * moveSpeed), ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            m_rigidBody.AddForce(new Vector3(transform.forward.x * runSpeed, 0, transform.forward.z * runSpeed), ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //m_rigidBody.AddForce(new Vector3(transform.right.x * moveSpeed, 0, transform.right.z * moveSpeed), ForceMode.Acceleration);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            //m_rigidBody.AddForce(new Vector3(-transform.right.x * moveSpeed, 0, -transform.right.z * moveSpeed), ForceMode.Acceleration);
+        }
+
+        // Movement Cap
+        if (m_rigidBody.velocity.magnitude > maxSpeed)
+        {
+            float f = m_rigidBody.velocity.magnitude - maxSpeed;
+            m_rigidBody.AddForce(m_rigidBody.velocity * -f);
+        }
+        //-------------------------------------------------------------------------------------------
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            m_rigidBody.transform.Rotate(new Vector3(0f, rotationSpeed, 0f));
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_rigidBody.transform.Rotate(new Vector3(0f, -rotationSpeed, 0f));
+            //m_rigidBody.AddTorque(0.0f, -rotationSpeed, 0.0f, ForceMode.Acceleration);
+            //Quaternion deltaRotation = Quaternion.Euler(m_EulerAngleVelocity * -rotationSpeed);
+            //m_rigidBody.MoveRotation(m_rigidBody.rotation * deltaRotation);
+        }
+    }
+}
