9e4ee4c6d023a2fbcca0dcd0884f2797f0a56bee
Nerbz <nathan.nette@gmail.com>
2018-06-20 00:04:49 +1000

    Added Post Proccessing
    Added Post proc and implemented them into the scene. The plan is to use buttons to turn each effect on or off.

---
+++ ComputerGraphicsProject/ProjectComputerGraphics/Assets/Scripts/Bloom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloom : MonoBehaviour {
+
+    [ExecuteInEditMode, ImageEffectAllowedInSceneView]
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination);
+    }
+}
