27bad41040bd335c68df3839f49186f19ac372e8
Nerbz <nathan.nette@gmail.com>
2018-06-20 22:02:17 +1000

    Shoots fireballs
    Does shoot fireballs. Very buggy, the position of where it shoots from doesn't move with the character.
    Currently instantiating the fireballs.

---
+++ ComputerGraphicsProject/ProjectComputerGraphics/Assets/Scripts/PlayerSpells.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpells : MonoBehaviour
+{
+    public GameObject fireBall;
+    public float delayForShoot;
+    public float shootPower;
+    //private float timer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Invoke("ShootFireball", delayForShoot);
+
+        }
+    }
+
+    private void ShootFireball()
+    {
+        GameObject fireball = Instantiate(fireBall, gameObject.transform.forward, Quaternion.identity);
+        fireball.GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * shootPower, ForceMode.Impulse);
+    }
+}
