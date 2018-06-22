01e084373eb86ae56aaeefca0eb939c0b494332e
Nerbz <nathan.nette@gmail.com>
2018-06-22 14:43:39 +1000

    Added procedurally generated terrain and attempt at shader
    The shader I was trying to get going changed the colour of the mesh depending on how tall it is in world space. This means that for the mountains, if they get to a certain height then they'd get white on the tops.

---
+++ ComputerGraphicsProject/ProjectComputerGraphics/Assets/MountainHeight.shader
@@ -0,0 +1,60 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/NathansShaders/MountainHeight"
+{
+	Properties
+	{
+		//White->Brown->Green
+		_PeakColour("Peak Colour") = 1,1,1,1
+		_MountainColour("Mountain Colour") = (139, 69, 19)
+	//_GrassColour("Grass Colour") = (71,33,0)
+		_GradientBlend("Gradient Blend", float) = 1
+	}
+
+		SubShader
+	{
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float3 worldPosition : TEXCOORD0;
+			};
+
+			float4 _PeakColour;
+			float4 _MountainColour;
+			float4 _GrassColour;
+			float _GradientBlend;
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.position = UnityObjectToClipPos(v.vertex);
+				o.worldPosition = mul(unity_ObjectToWorld, v.vertex).xyz;
+				return o;
+			}
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				float4 gradientColour = lerp(_MountainColour, _PeakColour, v.worldPosition * _GradientBlend);
+
+				return gradientColour;
+			}
+			ENDCG
+		}
+	}
+		FallBack "Diffuse"
+}
--- ComputerGraphicsProject/ProjectComputerGraphics/Assets/Scripts/ButtonFunctions.cs
+++ ComputerGraphicsProject/ProjectComputerGraphics/Assets/Scripts/ButtonFunctions.cs
@@ -4,17 +4,131 @@ using UnityEngine;
 
 public class ButtonFunctions : MonoBehaviour
 {
-
+    public GameObject postProcCamera;
+    private UnityEngine.PostProcessing.PostProcessingBehaviour postProcBehaviour;
     // Use this for initialization
     void Start()
     {
-
+        postProcBehaviour = postProcCamera.GetComponent<UnityEngine.PostProcessing.PostProcessingBehaviour>();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void OnFogClick()
+    {
+        if (!postProcBehaviour.profile.fog.enabled)
+            postProcBehaviour.profile.fog.enabled = true;
+        else
+            postProcBehaviour.profile.fog.enabled = false;
+    }
+
+
+    public void OnAntiAliasingClick()
+    {
+        if (!postProcBehaviour.profile.antialiasing.enabled)
+            postProcBehaviour.profile.antialiasing.enabled = true;
+        else
+            postProcBehaviour.profile.antialiasing.enabled = false;
+    }
+
+    public void OnAmbientOcclusionClick()
+    {
+        if (!postProcBehaviour.profile.ambientOcclusion.enabled)
+            postProcBehaviour.profile.ambientOcclusion.enabled = true;
+        else
+            postProcBehaviour.profile.ambientOcclusion.enabled = false;
+    }
+
+    public void OnScreenSpaceReflectionClick()
+    {
+        if (!postProcBehaviour.profile.screenSpaceReflection.enabled)
+            postProcBehaviour.profile.screenSpaceReflection.enabled = true;
+        else
+            postProcBehaviour.profile.screenSpaceReflection.enabled = false;
+    }
+
+    public void OnDepthOfFieldClick()
+    {
+        if (!postProcBehaviour.profile.depthOfField.enabled)
+            postProcBehaviour.profile.depthOfField.enabled = true;
+        else
+            postProcBehaviour.profile.depthOfField.enabled = false;
+    }
+
+    public void OnMotionBlurClick()
+    {
+        if (!postProcBehaviour.profile.motionBlur.enabled)
+            postProcBehaviour.profile.motionBlur.enabled = true;
+        else
+            postProcBehaviour.profile.motionBlur.enabled = false;
+    }
+
+    public void OnEyeAdaptionClick()
+    {
+        if (!postProcBehaviour.profile.eyeAdaptation.enabled)
+            postProcBehaviour.profile.eyeAdaptation.enabled = true;
+        else
+            postProcBehaviour.profile.eyeAdaptation.enabled = false;
+    }
+
+    public void OnBloomClick()
+    {
+        if (!postProcBehaviour.profile.bloom.enabled)
+            postProcBehaviour.profile.bloom.enabled = true;
+        else
+            postProcBehaviour.profile.bloom.enabled = false;
+    }
+
+    public void OnColorGradingClick()
+    {
+        if (!postProcBehaviour.profile.colorGrading.enabled)
+            postProcBehaviour.profile.colorGrading.enabled = true;
+        else
+            postProcBehaviour.profile.colorGrading.enabled = false;
+    }
+
+    public void OnUserLutClick()
+    {
+        if (!postProcBehaviour.profile.userLut.enabled)
+            postProcBehaviour.profile.userLut.enabled = true;
+        else
+            postProcBehaviour.profile.userLut.enabled = false;
+    }
+
+    public void OnChromaticAberrationClick()
+    {
+        if (!postProcBehaviour.profile.chromaticAberration.enabled)
+            postProcBehaviour.profile.chromaticAberration.enabled = true;
+        else
+            postProcBehaviour.profile.chromaticAberration.enabled = false;
+    }
+
+    public void OnGrainClick()
+    {
+        if (!postProcBehaviour.profile.grain.enabled)
+            postProcBehaviour.profile.grain.enabled = true;
+        else
+            postProcBehaviour.profile.grain.enabled = false;
+    }
+
+    public void OnVignetteClick()
+    {
+        if (!postProcBehaviour.profile.vignette.enabled)
+            postProcBehaviour.profile.vignette.enabled = true;
+        else
+            postProcBehaviour.profile.vignette.enabled = false;
+    }
+
+    public void OnDitheringClick()
+    {
+        if (!postProcBehaviour.profile.dithering.enabled)
+            postProcBehaviour.profile.dithering.enabled = true;
+        else
+            postProcBehaviour.profile.dithering.enabled = false;
     }
 
     public void OnExitClick()
